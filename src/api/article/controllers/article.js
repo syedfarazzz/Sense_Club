86be11ce663c48cb4df3c60696683afe9b880da5
Syed Faraz <syedfarazahmed.8@gmail.com>
2023-08-09 01:02:50 +0500

    Article-User relation created

--- src/api/article/controllers/article.js
+++ src/api/article/controllers/article.js
@@ -6,4 +6,40 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::article.article');
+module.exports = createCoreController('api::article.article', ({strapi}) => ({
+    
+
+    // customizing the create controller
+    async create(ctx, next)
+    {
+        // get user from context
+        const currentUser = ctx.state.user
+
+        // get request body data from context
+        const  {post_title, question, upload_pic, description, tags, social_link, likes, comments, categories}  = ctx.request.body.data
+
+        try
+        {
+        // use the create method from Strapi enitityService
+        const createdArticles = await strapi.entityService.create("api::article.article", 
+        {
+            data: 
+            {
+                post_title, question, upload_pic, description, tags, social_link, likes, comments, categories,
+
+                // pass in the publisher id to define the publisher
+                publisher: currentUser.id,
+            }
+        })
+
+            return {createdArticles}
+
+        }
+
+        catch(err)
+        {
+            console.log(err);
+        }
+    },
+
+}));
